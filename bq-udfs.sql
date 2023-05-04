ed8a62d3cd26d1d75efb3b8ca7d30195f0f2b85d
Vijay Lulla <vijay_lulla@sweetwater.com>
2023-05-04 13:32:07 -0400

    Added a few more BQ utility funcs.

--- bq-udfs.sql
+++ bq-udfs.sql
@@ -7,9 +7,24 @@
 create or replace function round2(x any type) as ( round(x, 2) );
 create or replace function round3(x any type) as ( round(x, 3) );
 
-create or replace function iso_yyyyweek(x any type) as ( cast(format_date("%G%V", cast(x as date) ) as int64) );
-create or replace function iso_yyyyday (x any type) as ( cast(format_date("%G%J", cast(x as date) ) as int64) ); -- 001-364 OR 001-371  !?!?!
-assert iso_yyyyday('2023-01-01') = 2022364; assert iso_yyyyday('2023-01-02') = 2023001;
+-- https://webspace.science.uu.nl/~gent0113/calendar/isocalendar.htm for iso calendar algorithms!
+
+-- [dw]oy: [day/week]-of-year
+create or replace function iso_yyyy_woy(x any type) as (cast(format_date("%G%V", cast(x as date)) as int64) ); -- https://en.wikipedia.org/wiki/ISO_week_date
+assert iso_yyyy_woy('2023-05-01') = 202318; assert mod(iso_yyyy_woy(current_date()),100) between 1 and 53;
+create or replace function iso_yyyy_doy (x any type) as (cast(format_date("%G%J", cast(x as date)) as int64) );
+assert iso_yyyy_doy ('2023-01-02') = 2023001;
+assert iso_yyyy_doy('2023-01-01') = 2022364; -- !!
+-- select d,iso_yyyy_woy(d) isowk,iso_yyyy_doy(d) isoday from unnest(generate_date_array('2023-01-01','2023-12-31',interval 1 day)) as d;
+
+create or replace function lastarrayelement(arr any type) as ( arr[safe_ordinal(array_length(arr))]);
+select lastarrayelement(a) from (select [1,2,3,4] as a union all select [] union all select [1]) s;
+-- format-bq-query removes lines beginning with assert! Therefore, please keep assert[s] on one line!
+assert lastarrayelement([1,2,3,4]) = 4;
+assert lastarrayelement([]) is null;
+assert lastarrayelement([1]) = 1;
+assert lastarrayelement([struct(1 as x, 2 as y),(11,12),(21,22),(31,32)]) = (31,32);
+-- select lastarrayelement(15) should raise error! Since SQL is strongly typed arr can be only ARRAY type! Therefore BQ will raise an error here! SQL is awesome!
 
 create or replace function deg2rad(deg any type) as ( deg * acos(-1) / 180 ); assert deg2rad(180) == acos(-1); -- acos(-1) is pi!
 create or replace function rad2deg(rad any type) as ( rad * 180 / acos(-1) ); assert rad2deg(acos(-1)) == 180;
