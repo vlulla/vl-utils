ab34dbf01c6601a9802fab20f8713ac3dbadff78
Vijay Lulla <vijay_lulla@sweetwater.com>
2024-10-04 11:11:21 -0400

    typo

--- duckdb-utils.sql
+++ duckdb-utils.sql
@@ -1,5 +1,5 @@
 -- bash $ duckdb
--- D read 'utils.sql'
+-- D .read 'utils.sql'
 -- D
 
 -- select yqtr,count(*),min(dt) first_dt,max(dt) as last_dt from (select dt,yearqtr(dt) as yqtr from (select unnest(generate_series('2024-01-01'::date,'2024-12-31',interval 1 day))::date as dt)) group by all order by yqtr;
