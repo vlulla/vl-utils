fd8a0341670ef2a3718ef555118b037ff7f99d97
Vijay Lulla <vijay_lulla@sweetwater.com>
2024-11-15 17:24:12 -0500

    Added a macro to generate calendar between custom dates.
    This simplifies some of the more esoteric explorations that i undertake.

--- duckdb-utils.sql
+++ duckdb-utils.sql
@@ -4,5 +4,7 @@
 
 -- select yqtr,count(*),min(dt) first_dt,max(dt) as last_dt from (select dt,yearqtr(dt) as yqtr from (select unnest(generate_series('2024-01-01'::date,'2024-12-31',interval 1 day))::date as dt)) group by all order by yqtr;
 create macro yearqtr(x) as (select (date_part('year', x::date)*10+((date_part('month', x::date)-1) // 3) + 1));
+-- select * from calendar_between_dates('2024-06-15','2025-02-13'); -- calendar dates between custom dates!!
+create macro calendar_between_dates(start_date,end_date) as table (with _ as (select unnest(generate_series(start_date::date,end_date::date,interval 1 day)::date[])::date as dt) select dt,strftime(dt,'%a') as dow, yearqtr(dt) as YYYYQTR from _);
 -- select * from calendar_year(2024);
-create macro calendar_year(yr) as TABLE (with _ as (select DT::DATE as DT from unnest(generate_series(cast(YR || '-01-01' as date), cast(YR || '-12-31' as date), interval '1' day)::date[]) as _(dt)) select dT, strftime(dt, '%a') as dow, yearqtr(dt) as YYYYQTR from _);
+create macro calendar_year(yr) as TABLE (select * from calendar_between_dates(yr||'-01-01', yr||'-12-31'));
