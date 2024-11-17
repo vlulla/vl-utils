fc0c49eac5e8468880e3d0cb45f2646d03bdac58
Vijay Lulla <vijaylulla@gmail.com>
2024-11-17 06:42:43 -0500

    Added a macro to generate random date range

--- duckdb-utils.sql
+++ duckdb-utils.sql
@@ -8,3 +8,7 @@ create macro yearqtr(x) as (select (date_part('year', x::date)*10+((date_part('m
 create macro calendar_between_dates(start_date,end_date) as table (with _ as (select unnest(generate_series(start_date::date,end_date::date,interval 1 day)::date[])::date as dt) select dt,strftime(dt,'%a') as dow, yearqtr(dt) as YYYYQTR from _);
 -- select * from calendar_year(2024);
 create macro calendar_year(yr) as TABLE (select * from calendar_between_dates(yr||'-01-01', yr||'-12-31'));
+-- select * from random_date_range(10);
+-- select * from random_date_range(floor(365*random())::int); -- even better!
+-- with _ as (select * from random_date_range(floor(365*random())::int)) select length(dates) as len,dates from _;
+create macro random_date_range(ndays) as TABLE (with _ as (select '2000-01-01'::date + floor((current_date() - '2000-01-01'::date)*random())::int as start_date) select generate_series(start_date,start_date + ndays,interval 1 day)::date[] as dates from _);
