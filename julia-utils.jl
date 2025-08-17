74e7ddde560a1e90640744c03180c994ef13a875
Vijay Lulla <vijaylulla@gmail.com>
2025-08-17 11:02:08 -0400

    Decided to start using Julia where i'd normally use R

---
+++ julia-utils.jl
@@ -0,0 +1,14 @@
+using Pkg
+
+function install_pkgs(pkgs...)
+  Pkg.update()
+  installedpkgs = [x.name for x in values(Pkg.dependencies())]
+  missingpkgs = setdiff(pkgs, installedpkgs)
+  if length(missingpkgs)>0
+    println("Installing $(missingpkgs)")
+    Pkg.add(missingpkgs)
+  end
+end
+## Some of the packages i commonly use...
+## install_pkgs("DataFrames","Distributions","GLM","MixedModels","Plots","StatsBase","RDatasets")
+
