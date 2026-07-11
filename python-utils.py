964fd9e87542ce85fc6a8bb010464401926630a0
Vijay Lulla <vijaylulla@gmail.com>
2026-07-11 09:54:00 -0400

    Did not handle strings correctly...oops!

--- python-utils.py
+++ python-utils.py
@@ -582,8 +582,8 @@ def sublists(l: collections.abc.Iterable[T], n: int=1) -> collections.abc.Iterab
   """
   assert isinstance(n, int) and n>0
   if not isinstance(l, collections.abc.Sequence): raise TypeError(f"Unsupported {type(l)}. Only works for collections.abc.Sequence. So, won't work for np.ndarray too!")
-  if len(l) < n: return type(l)([l])
   rettype = identity if isinstance(l, str) else type(l)
+  if len(l) < n: return rettype([l])
   return rettype([l[i:i+n] for i in range(len(l)-n+1)])
 
 def idir():
